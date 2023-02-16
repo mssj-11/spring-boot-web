b9236e2656dd0c54c86cf82aff45fc528d383bc8
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-15 23:06:09 -0600

    Obtener varios parametros de la URL con @RequestPa

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 //	Nuevo controlador
 @Controller
 @RequestMapping("/params")	//	Ruta base
@@ -29,6 +31,36 @@ public class EjemploParamController {
 		return "params/ver";	//	Carpeta/Plantilla HTML
 	}
 	
+	
+	//	Metodo, obtener varios parametros de URL con @RequestParam
+	@GetMapping("/varios-params")	//	Metodo GET, Mapear con tipo de dato String al controlador
+	//	Parametros mediante URL-ruta, Modelo
+	public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+		model.addAttribute("resultado", "El texto enviado es: '" +saludo+ "' y el numero es '" +numero+ "'");
+		
+		return "params/ver";	//	Carpeta/Plantilla HTML
+	}
+	
+	
+//	Otro Metodo, para obtener varios parametros de URL pero ahora con HttpServletRequest
+	@GetMapping("/varios-params-request")	//	Metodo GET, Mapear con tipo de dato String al controlador
+	//	Parametros HttpServletRequest, Modelo
+	public String param(HttpServletRequest request, Model model) {
+		//	Obtenidno el String=saludo y Integer=numero
+		String saludo = request.getParameter("saludo");
+		
+		Integer numero = null;	//	numero inicializado en NULL
+		//	Manejo de errores por valores en Cadena y que no son Enteros
+		try {
+		numero = Integer.parseInt(request.getParameter("numero"));
+		}catch(NumberFormatException e) {
+			numero = 0;	// Se puede dejar vacio
+		}
+		
+		model.addAttribute("resultado", "El texto enviado es: '" +saludo+ "' y el numero es '" +numero+ "'");
+		
+		return "params/ver";	//	Carpeta/Plantilla HTML
+	}
 	
 	
 	
