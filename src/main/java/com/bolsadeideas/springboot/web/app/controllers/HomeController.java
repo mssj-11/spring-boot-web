93af40bb595dbb8975b6435eb84539da697d1d81
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-18 17:24:25 -0600

    Retornando redirect y forward
    Retornando redirect y forward como respuesta a metodos del controlador

---
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	
+	//	Metodo Redirect
+	@GetMapping("/")	//	Anotacion @GetMapping
+	public String home() {
+		//return "redirect:/app/index";
+		//return "redirect:https://www.google.com";	//	A otra Ruta
+		return "forward:/app/index";	//	Solo rutas de controladores, no se puede con externas
+	}
+	
+	
+
+}
